e177ddb24fad86da747f1e9524e88c78d48a44f1
Qiuxsi <1030811033@qq.com>
2023-12-13 02:26:14 +0800

    framework

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view/>
+  <div>
+    <router-view/>
+  </div>
 </template>
 
 <style>
@@ -10,17 +12,9 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
