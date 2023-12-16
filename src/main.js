6a5266971a01245218593a884d70e423008d62e0
Qiuxsi <1030811033@qq.com>
2023-12-16 14:21:19 +0800

    npm install

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 
+import VueClipboard from 'vue-clipboard2'
+
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(ElementPlus)
+app.use(VueClipboard)
 app.mount('#app')
